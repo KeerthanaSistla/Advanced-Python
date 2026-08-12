08b47c5330e9fa8c8123739bd0821ebb7875b4c0
Sistla Keerthana <keerthanasistla10@gmail.com>
2026-08-12 22:07:50 +0530

    Project 1 v2

--- Project1/main.py
+++ Project1/main.py
@@ -1,23 +1,43 @@
-class Rectangle:
-    def __init__(self, lowleft, upright):
-        self.lowleft = lowleft
-        self.upright = upright
-
-
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def falls_in_rectangle(self, rectangle):
-        if (rectangle.lowleft[0] < self.x < rectangle.upright[0]
-                and rectangle.lowleft[1] < self.y < rectangle.upright[1]):
+        if (rectangle.lowleft.x < self.x < rectangle.upright.x
+                and rectangle.lowleft.y < self.y < rectangle.upright.y):
             return True
         else:
             return False
 
 
-point1 = Point(6, 7)
-rectangle1 = Rectangle((5, 6), (10, 12))
+class Rectangle:
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
 
-print(point1.falls_in_rectangle(rectangle1))
+
+from random import randint
+
+rectangle = Rectangle(
+    Point(randint(0, 9), randint(0, 9)),
+    Point(randint(10, 19), randint(10, 19))
+)
+
+print(
+    "Rectangle Coordinates: ",
+    rectangle.lowleft.x, ",",
+    rectangle.lowleft.y, "and",
+    rectangle.upright.x, ",",
+    rectangle.upright.y
+)
+
+user_point = Point(
+    float(input("Guess X: ")),
+    float(input("Guess Y: "))
+)
+
+print(
+    "Your point falls in rectangle: ",
+    user_point.falls_in_rectangle(rectangle)
+)
