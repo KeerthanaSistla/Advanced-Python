f485aaca78115c50b05fd8e8c71c89aa866f35d9
Sistla Keerthana <keerthanasistla10@gmail.com>
2026-08-12 22:55:23 +0530

    Project 2 v4

--- 2. Flatmates' Bill/fpdf_test.py
+++ 2. Flatmates' Bill/fpdf_test.py
@@ -5,6 +5,7 @@ pdf.add_page()
 
 pdf.set_font(family='Times', size=24, style='B')
 pdf.cell(w=100, h=80, txt='Flatmates Bill', border=1, ln=1, align='C')
-pdf.cell(w=50, h=40, txt='Period', border=1)
+pdf.cell(w=100, h=40, txt='Period', border=1)
+pdf.cell(w=150, h=40, txt='March 2021', border=1, ln=1)
 
 pdf.output("2. Flatmates' Bill\\bill.pdf")
