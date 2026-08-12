dc924b65ac0922ceb1f2df246e30ccc58dff6f34
Sistla Keerthana <keerthanasistla10@gmail.com>
2026-08-12 22:50:37 +0530

    Project 2 v3

---
+++ 2. Flatmates' Bill/fpdf_test.py
@@ -0,0 +1,10 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.add_page()
+
+pdf.set_font(family='Times', size=24, style='B')
+pdf.cell(w=100, h=80, txt='Flatmates Bill', border=1, ln=1, align='C')
+pdf.cell(w=50, h=40, txt='Period', border=1)
+
+pdf.output("2. Flatmates' Bill\\bill.pdf")
