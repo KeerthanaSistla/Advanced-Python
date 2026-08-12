df8541af090952b69d9c3e6b563f99b5e9ea43b4
Sistla Keerthana <keerthanasistla10@gmail.com>
2026-08-12 22:59:43 +0530

    Project 2 v5

--- 2. Flatmates' Bill/main.py
+++ 2. Flatmates' Bill/main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     def __init__(self, amount, period):
         self.amount = amount
@@ -22,7 +24,16 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        pdf = FPDF(orientation='P', unit='mm', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=100, h=80, txt='Flatmates Bill', border=1, ln=1, align='C')
+        pdf.cell(w=100, h=40, txt='Period', border=1)
+        pdf.cell(w=150, h=40, txt='March 2021', border=1, ln=1)
+
+        pdf.output("2. Flatmates' Bill\\bill.pdf")
 
 
 the_bill = Bill(amount=120, period="March 2021")
@@ -32,3 +43,6 @@ mary = Flatmate(name="Mary", days_in_house=25)
 
 print(john.pays(bill=the_bill, flatmate2=mary))
 print(mary.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="bill.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
