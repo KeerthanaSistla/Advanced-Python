c6f0a74a571b2c9933ebd792fb71b248607cff61
Sistla Keerthana <keerthanasistla10@gmail.com>
2026-08-12 21:56:59 +0530

    Initial commit

---
+++ Project1/main.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft[0] < self.x < rectangle.upright[0] and rectangle.lowleft[1] < self.y < rectangle.upright[1]:
+            return True
+        else:
+            return False
+
+point1 = Point(6, 7)
+point1.falls_in_rectangle((5,6), (10,12))
